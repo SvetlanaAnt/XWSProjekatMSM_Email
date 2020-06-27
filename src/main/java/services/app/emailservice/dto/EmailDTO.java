f50694347c44bdd8ef56e835b8231170182e0d50
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-06-27 23:07:12 +0200

    Refactoring mail service

--- src/main/java/services/app/emailservice/dto/EmailDTO.java
+++ src/main/java/services/app/emailservice/dto/EmailDTO.java
@@ -2,13 +2,15 @@ package services.app.emailservice.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Builder
 @ToString
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmailDTO {
+public class EmailDTO implements Serializable {
     String email;
     String subject;
     String message;
