26646dcc87123c27f5191c335981d82af45636d9
Miroslav Tomic <56967120+tmiroslav97@users.noreply.github.com>
2020-06-28 20:13:16 +0200

    Fixing bugs

--- src/main/java/services/app/emailservice/service/impl/EmailServiceImpl.java
+++ src/main/java/services/app/emailservice/service/impl/EmailServiceImpl.java
@@ -1,15 +1,19 @@
 package services.app.emailservice.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import services.app.emailservice.conf.RabbitMQConfiguration;
 import services.app.emailservice.dto.EmailDTO;
 import services.app.emailservice.service.intf.EmailService;
 
+@Service
 public class EmailServiceImpl implements EmailService {
 
     @Autowired
@@ -21,13 +25,19 @@ public class EmailServiceImpl implements EmailService {
     @Override
     @Async
     @RabbitListener(queues = RabbitMQConfiguration.QUEUE_NAME)
-    public void sendMailTo(EmailDTO emailDTO) {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(emailDTO.getEmail());
-        mail.setFrom(environment.getProperty("spring.mail.username"));
-        mail.setSubject(emailDTO.getSubject());
-        mail.setText(emailDTO.getMessage());
-        javaMailSender.send(mail);
+    public void sendMailTo(String msg) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            EmailDTO emailDTO = objectMapper.readValue(msg, EmailDTO.class);
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(emailDTO.getEmail());
+            mail.setFrom(environment.getProperty("spring.mail.username"));
+            mail.setSubject(emailDTO.getSubject());
+            mail.setText(emailDTO.getMessage());
+            javaMailSender.send(mail);
+        } catch (JsonProcessingException exception) {
+            return;
+        }
 
     }
 }
--- src/main/java/services/app/emailservice/service/intf/EmailService.java
+++ src/main/java/services/app/emailservice/service/intf/EmailService.java
@@ -1,7 +1,8 @@
 package services.app.emailservice.service.intf;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import services.app.emailservice.dto.EmailDTO;
 
 public interface EmailService {
-    public void sendMailTo(EmailDTO emailDTO);
+    public void sendMailTo(String msg);
 }
